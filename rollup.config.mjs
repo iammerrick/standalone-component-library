60a620e2a66a01fbcf2e2b80dbb2ec07030c9670
iammerrick <merrick.christensen@gmail.com>
2025-03-20 15:42:59 -0600

    bundle

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import typescript from "@rollup/plugin-typescript";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "src/index.ts",
@@ -11,6 +12,12 @@ export default {
   },
 
   plugins: [
+    replace({
+      preventAssignment: true,
+      values: {
+        "process.env.NODE_ENV": JSON.stringify("production"),
+      },
+    }),
     resolve(),
     commonjs(),
     typescript({
