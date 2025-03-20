5bb4c73a0f8c9969ed1067d686728e20dd4be7aa
iammerrick <merrick.christensen@gmail.com>
2025-03-20 13:10:22 -0600

    default

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export * as React from "react";
 export * as ReactDOM from "react-dom/client";
-import Button from "./Button.js";
+import * as Button from "./Button.js";
 export const modules = {
   Button,
 };
