0ff8914564cdb4f27e7189294d177d900969daf6
iammerrick <merrick.christensen@gmail.com>
2025-03-20 15:15:47 -0600

    default component

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 export * as React from "react";
 export * as ReactDOM from "react-dom/client";
 import * as Button from "./Button.js";
-export const components = {
-  Button,
-};
+export default Button;
